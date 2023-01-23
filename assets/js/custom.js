afd982ad9bff9d064cb0146c6032d05f0bd6b22d
Cobbina Emmanuel <kobbyj099@gmail.com>
2023-01-23 06:18:10 -0800

    Called the products API

---
+++ assets/js/custom.js
@@ -0,0 +1,69 @@
+// TABLE OF CONTENTS
+
+//
+
+
+// Shop
+
+const products_row = document.querySelector('.products-row')
+
+
+async function getAllProducts() {
+    const url = "https://dummyjson.com/products";
+    const req = await fetch(url);
+    const res = await req.json();
+    const products = res.products;
+    console.log(products)
+    products.forEach(product => {
+        console.log(product.title);
+        if (
+          !(
+            product.title === "Handcraft Chinese style" ||
+            product.title === "cereals muesli fruit nuts"
+          )
+        ) {
+          products_row.innerHTML += `
+        <div class="col-md-4">
+                        <div class="card mb-4 product-wap rounded-0">
+                            <div class="card rounded-0">
+                                <img class="card-img rounded-0 img-fluid" src="${product.thumbnail}">
+                                <div class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
+                                    <ul class="list-unstyled">
+                                        <li><a class="btn btn-success text-white" href="shop-single.html"><i class="far fa-heart"></i></a></li>
+                                        <li><a class="btn btn-success text-white mt-2" href="shop-single.html"><i class="far fa-eye"></i></a></li>
+                                        <li><a class="btn btn-success text-white mt-2" href="shop-single.html"><i class="fas fa-cart-plus"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <a href="shop-single.html" class="h3 text-decoration-none">${product.title}</a>
+                                <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
+                                    <li>Brand: ${product.brand}</li>
+                                    <li class="pt-2">
+                                        <span class="product-color-dot color-dot-red float-left rounded-circle ml-1"></span>
+                                        <span class="product-color-dot color-dot-blue float-left rounded-circle ml-1"></span>
+                                        <span class="product-color-dot color-dot-black float-left rounded-circle ml-1"></span>
+                                        <span class="product-color-dot color-dot-light float-left rounded-circle ml-1"></span>
+                                        <span class="product-color-dot color-dot-green float-left rounded-circle ml-1"></span>
+                                    </li>
+                                </ul>
+                                <ul class="list-unstyled d-flex justify-content-center mb-1">
+                                    <li>
+                                        <i class="text-warning fa fa-star"></i>
+                                        <i class="text-warning fa fa-star"></i>
+                                        <i class="text-warning fa fa-star"></i>
+                                        <i class="text-muted fa fa-star"></i>
+                                        <i class="text-muted fa fa-star"></i>
+                                    </li>
+                                </ul>
+                                <p class="text-center mb-0">$${product.price}</p>
+                            </div>
+                        </div>
+                    </div>
+        `;
+        }
+     
+    });
+}
+getAllProducts();
+
