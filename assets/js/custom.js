b6250f0322e19677c3688233ac29feba4027eabd
Cobbina Emmanuel <kobbyj099@gmail.com>
2023-01-24 20:58:42 -0800

    Added products for each category

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,6 +9,7 @@ const products_row = document.querySelector('.products-row')
 let productsUrl = "https://dummyjson.com/products";
 const categories = document.querySelectorAll('.cat')
 const nextButtons = document.querySelectorAll('.next')
+const categoryGroups = document.querySelectorAll(".categories-product");
 
 categories.forEach(button => {
     if (button.textContent.includes('All')) {
@@ -20,8 +21,28 @@ categories.forEach(button => {
         nextButtons.forEach(nextBtn => {
             if (nextBtn.textContent.includes('2')) {
                 nextBtn.addEventListener('click', function () {
+                    nextBtn.style.backgroundColor = 'green';
+                    nextButtons[0].style.backgroundColor = 'white'
+                    nextButtons[2].style.backgroundColor = 'white'
                     nextOne();
-                })
+                });
+            } else if (nextBtn.textContent.includes('3')) {
+                nextBtn.addEventListener("click", function () {
+                  nextBtn.style.backgroundColor = "green";
+                  nextButtons[0].style.backgroundColor = "white";
+                  nextButtons[1].style.backgroundColor = "white";
+                  nextTwo();
+                });
+            } else if (nextBtn.textContent.includes('1')) {
+                nextBtn.addEventListener("click", function () {
+                  nextBtn.style.backgroundColor = "green";
+                  nextButtons[1].style.backgroundColor = "white";
+                    nextButtons[2].style.backgroundColor = "white";
+                    products_row.innerHTML = '';
+                    productsUrl = "https://dummyjson.com/products";
+                    getProducts();
+                    tops();
+                });
             }
         })
     } else if (button.textContent.includes("Men's")) {
@@ -45,6 +66,41 @@ categories.forEach(button => {
         })
     }
 });
+
+categoryGroups.forEach(itemGroup => {
+    if (itemGroup.textContent.includes("Furnitures")) {
+        itemGroup.addEventListener("click", function () {
+        products_row.textContent = "";
+        furniture();
+      });
+    } else if (itemGroup.textContent.includes("Skincare")) {
+      itemGroup.addEventListener("click", function () {
+        products_row.textContent = "";
+        skincare();
+      });
+    } else if (itemGroup.textContent.includes("Sunglasses")) {
+      itemGroup.addEventListener("click", function () {
+        products_row.textContent = "";
+        sunglasses();
+      });
+    } else if (itemGroup.textContent.includes("Lighting")) {
+      itemGroup.addEventListener("click", function () {
+        products_row.textContent = "";
+        lighting();
+      });
+    } else if (itemGroup.textContent.includes("Automotive")) {
+      itemGroup.addEventListener("click", function () {
+        products_row.textContent = "";
+        automotive();
+      });
+    } else if (itemGroup.textContent.includes("Smartphones and Laptops")) {
+      itemGroup.addEventListener("click", function () {
+        products_row.textContent = "";
+          laptops();
+          smartphones();
+      });
+    }
+})
 
 async function getProducts() {
     
@@ -181,21 +237,48 @@ function tops() {
     productsUrl = "https://dummyjson.com/products/category/tops";
     moreProducts();
 }
-function homeDecor() {
-    productsUrl = "https://dummyjson.com/products/category/home-decoration";
+function automotive() {
+    productsUrl = "https://dummyjson.com/products/category/automotive";
     moreProducts();
 }
 function nextOne() {
-    products_row.textContent = ''
-    productsUrl = "https://dummyjson.com/products/category/groceries";
+    products_row.textContent = '';
+    productsUrl = "https://dummyjson.com/products/category/motorcycle";
     moreProducts();
-    homeDecor();
+    automotive();
+    lighting();
 }
 function nextTwo() {
-    productsUrl = "https://dummyjson.com/products/category/tops";
+    products_row.innerHTML = '';
+    productsUrl = "https://dummyjson.com/products/category/sunglasses";
+    moreProducts();
+    furniture();
+}
+function furniture() {
+    productsUrl = "https://dummyjson.com/products/category/furniture";
     moreProducts();
 }
-function nextThree() {
-    productsUrl = "https://dummyjson.com/products/category/tops";
+function lighting() {
+    productsUrl = "https://dummyjson.com/products/category/lighting";
     moreProducts();
+}
+function smartphones() {
+    productsUrl = "https://dummyjson.com/products/category/smartphones";
+    moreProducts();
+}
+function laptops() {
+    productsUrl = "https://dummyjson.com/products/category/laptops";
+    moreProducts();
+}
+function skincare() {
+    productsUrl = "https://dummyjson.com/products/category/skincare";
+    moreProducts();
+}
+function sunglasses() {
+    productsUrl = "https://dummyjson.com/products/category/sunglasses";
+    moreProducts();
+}
+
+function onmouseOver() {
+  backgroundColor = "green";
 }
